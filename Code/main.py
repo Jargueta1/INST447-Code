5e1d3676f6b43baacca6b4647ce1e2ed607569ba
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2022-10-18 10:45:12 -0400

    ingested data and deleted fields not needed

---
+++ Code/main.py
@@ -0,0 +1,39 @@
+'''
+Name: Main.py
+Author: Jorge Argueta 
+Class: INST 447
+Descreption: extract data from csv files 
+    '''
+
+import pandas as pd 
+
+
+def getData(path):
+    """
+    description: takes file path and imports it as a df 
+
+    input: file path
+    """
+    df = pd.read_csv(path) ## gets data from CSV and adds it to pd df 
+    
+    df = df.set_index("datetime")
+    
+    ## Removing columns not needed
+    data_not_needed = ["name", "sunrise","sunset","conditions","description","icon","stations","severerisk"]
+    
+    for s in data_not_needed:
+        df.pop(s)
+    
+    
+    return df
+
+
+def main(): 
+    
+    df = getData("raw_data/Washington,DC,USA 2021-01-01 to 2021-12-31.csv")
+    
+    print(df.head())
+
+    print("done")
+    
+main()
